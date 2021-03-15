bb6a1b365b306349710c53bca5436e7affb73ace
DESKTOP-RDGJIOL\poloz <polozhenskii@gmail.com>
2021-03-15 03:08:38 +0300

    Add some scripts for account and language menu

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const resultItems = document.querySelectorAll(".results__item");
 const itemInfos = document.querySelectorAll(".item-info");
 
+// pop up windows
 for (let i = 0; i < resultItems.length; i++) {
   resultItems[i].id = `r${i}`;
 
@@ -24,3 +25,61 @@ for (let i = 0; i < resultItems.length; i++) {
     itemInfos[i].style.visibility = "hidden";
   });
 }
+
+// account menu
+const account = document.querySelector(".profile__account");
+const accountMenu = document.querySelector(".profile__account_menu");
+let isAccountMenuVisible = false;
+
+account.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (!isAccountMenuVisible) {
+    accountMenu.style.opacity = "1";
+    accountMenu.style.visibility = "visible";
+    isAccountMenuVisible = true;
+  } else {
+    accountMenu.style.opacity = "0";
+    accountMenu.style.visibility = "hidden";
+    isAccountMenuVisible = false;
+  }
+});
+
+// language menu
+const language = document.querySelector(".language");
+const languageMenu = document.querySelector(".language__menu");
+let isLanguageMenuVisible = false;
+
+language.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (!isLanguageMenuVisible) {
+    languageMenu.style.opacity = "1";
+    languageMenu.style.visibility = "visible";
+    isLanguageMenuVisible = true;
+  } else {
+    languageMenu.style.opacity = "0";
+    languageMenu.style.visibility = "hidden";
+    isLanguageMenuVisible = false;
+  }
+});
+
+document.onclick = function (e) {
+  if (
+    !e.target.classList.contains("profile__account_name") &&
+    !e.target.classList.contains("profile__arrow-down_account")
+  ) {
+    accountMenu.style.opacity = "0";
+    accountMenu.style.visibility = "hidden";
+    isAccountMenuVisible = false;
+  }
+
+  if (
+    !e.target.classList.contains("flag") &&
+    !e.target.classList.contains("profile__arrow-down_language")
+  ) {
+    languageMenu.style.opacity = "0";
+    languageMenu.style.visibility = "hidden";
+    isLanguageMenuVisible = false;
+  }
+};
